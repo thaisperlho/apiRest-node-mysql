acc51c85a6f26e89524a275ecfc4bc761a8c1354
thaisperlho <carvalho.thais0301@gmail.com>
2023-09-25 11:50:41 -0300

    update editoraController

--- controllers/editoraController.js
+++ controllers/editoraController.js
@@ -7,34 +7,42 @@ const Editora = require('../models').Editora;
 
 //Busca Editoras (GET)
 router.get('/', async (req, res) => {
-const editoras = await Editora.findAll(); res.status(200).json(editoras);
+        const editoras = await Editora.findAll();
+        res.status(200).json(editoras);
 });
 
 //Cadastra Editoras (POST)
 router.post('/', async (req, res) => {
-const {descricao} = req.body;
-const newEdit = await Editora.create({descricao})
-res.status(200).json({message: 'Cadastrado com sucesso'}); });
+        const { descricao } = req.body;
+        const newEdit = await Editora.create({ descricao })
+        res.status(200).json({ message: 'Cadastrado com sucesso' });
+});
 
 //Busca Por id a Editora (GET)
 router.get('/:id', async (req, res) => {
-const id=req.params;
-const editora = await Editora.findByPk(req.params.id); res.status(200).json(editora);
+        const id = req.params;
+        const editora = await Editora.findByPk(req.params.id);
+        res.status(200).json(editora);
 });
 
 //Deleta editora por id (DELETE)
-router.delete('/:id', async (req, res) =>{ await Editora.destroy({
-where:{
-id: req.params.id,
-}, });
-res.status(200).json({message:'Excluído com sucesso'}) });
+router.delete('/:id', async (req, res) => {
+        await Editora.destroy({
+                where: {
+                        id: req.params.id,
+                },
+        });
+        res.status(200).json({ message: 'Excluído com sucesso' })
+});
 
 //Altera Editora por ID (PUT)
-router.put('/:id', async (req, res) =>{ const {descricao} = req.body;
-await Editora.update(
-        { descricao},
-{
-where: {id:req.params.id},
-} );
-res.status(200).json({message: 'Atualizado com sucesso'}); });
-module.exports=router;
+router.put('/:id', async (req, res) => {
+        const { descricao } = req.body;
+        await Editora.update(
+                { descricao },
+                {
+                        where: { id: req.params.id },
+                });
+        res.status(200).json({ message: 'Atualizado com sucesso' });
+});
+module.exports = router;
